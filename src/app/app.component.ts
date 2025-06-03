8662d9c3db68f3a1be0d7edb658214ce36a2b993
Bit by Bit <162353202+GitClownBOJ@users.noreply.github.com>
2025-06-03 21:36:17 +0300

    Update app.component.ts routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router'; // For the <router-outlet>
+import { CommonModule } from '@angular/common';
 import { HamburgerMenuComponent } from './hamburger-menu/hamburger-menu.component'; // Import your UI components
 import { NavigationComponent } from './navigation/navigation.component';
 import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
 import { GifOverlayComponent } from './gif-overlay/gif-overlay.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true, // Mark app.component as standalone
-  imports: [RouterOutlet, HamburgerMenuComponent, NavigationComponent, MobileNavComponent], // Import other standalone components
+  imports: [RouterOutlet, CommonModule, HamburgerMenuComponent, NavigationComponent, MobileNavComponent], // Import other standalone components
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ShockBliss';
+    isShopRoute: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Subscribe to router events to update isShopRoute
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isShopRoute = event.urlAfterRedirects === '/shop';
+    });
+  }
+
 
   onMenuToggle(isOpen: boolean) {
     console.log('Hamburger menu is now:', isOpen ? 'open' : 'closed');
